9cfebfb3840ae1a2d34414a673712f5329c84bac
Bill Agee <billagee@gmail.com>
2015-11-08 14:59:36 -0800

    Bash and jq JSON iteration example

---
+++ bash/iterate_over_json_object.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+#
+# Illustrates how to use jq from a bash script to iterate over
+# the keys in a JSON object.
+
+set -e
+
+# Write a JSON file to disk that contains values to read later:
+JSON_FILENAME=foo-data.json
+
+cat << JSON_EOF > $JSON_FILENAME
+{
+  "foo": {
+    "a_string": "foostring",
+    "an_int": 1,
+    "an_optional_value": 2000000
+  },
+  "bar": {
+    "a_string": "barstring",
+    "an_int": 2,
+    "an_optional_value": null
+  },
+  "baz": {
+    "a_string": "bazstring",
+    "an_int": 3
+  }
+}
+JSON_EOF
+
+echo "jq version is $(jq --version)"
+
+# Read all the JSON object's keys into a bash array
+an_array=( $(jq -r 'keys[]' ${JSON_FILENAME}) )
+#printf "%s\n" ${an_array[@]}  # print keys
+#echo $(jq . ${JSON_FILENAME}) # print entire object
+
+# Use the JSON values to build a command string to eval, then store
+# the output of each command in an array:
+OUTPUT_ARRAY=()
+
+for the_key in ${an_array[@]}; do
+    # Assign applet input vars using JSON data
+    a_string=$(jq -r .$the_key.a_string ${JSON_FILENAME})
+    #echo "a_string is: '${a_string}'"
+    an_int=$(jq -r .$the_key.an_int ${JSON_FILENAME})
+    an_optional_value=$(jq -r .$the_key.an_optional_value ${JSON_FILENAME})
+
+    #echo "Building command string for $the_key..."
+    cmd_to_run="echo the_key: $the_key ... a_string: $a_string ... an_int: $an_int"
+
+    if [ $an_optional_value != "null" ] ; then
+        cmd_to_run="${cmd_to_run} ... an_optional_value: ${an_optional_value}"
+    fi
+
+    # Execute command
+    cmd_output=$(eval "${cmd_to_run}")
+    #echo "cmd_output is: '${cmd_output}'"
+
+    # Save output
+    # NOTE - without quoting $cmd_output below, you'll end up appending many
+    # array elements, since the string will be split on whitespace:
+    OUTPUT_ARRAY+=("$cmd_output")
+done
+
+echo "Output array len: ${#OUTPUT_ARRAY[@]}"
+
+# Print entire array contents using printf
+#printf '%s\n' "${OUTPUT_ARRAY[@]}"
+
+# Or using for, resetting IFS to prevent the loop from splitting on the
+# whitespace found in the elements:
+: <<'END_COMMENT'
+IFS=$'\n'
+for i in ${OUTPUT_ARRAY[@]}; do
+    echo "****"
+    echo ${i}
+done
+unset IFS
+END_COMMENT
+
+# Using the array index prevents whitespace splitting:
+for ((i = 0; i < ${#OUTPUT_ARRAY[@]}; i++)); do
+    echo "**** command output string #$i was: ***"
+    echo "${OUTPUT_ARRAY[i]}"
+done
+
