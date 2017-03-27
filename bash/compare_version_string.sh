1e78ae2afcb10e9652e6797fd1f19db0819a88a1
Bill Agee <billagee@gmail.com>
2017-03-27 13:23:50 -0700

    Updating R version string checker

--- bash/compare_version_string.sh
+++ bash/compare_version_string.sh
@@ -2,11 +2,12 @@
 #set -x
 
 #R_VERSION_EXPECTED="R version 3.0.3 (2014-03-06) -- \"Warm Puppy\""
-R_VERSION_EXPECTED="R version 3.2.0 (2015-04-16) -- \"Full of Ingredients\""
+#R_VERSION_EXPECTED="R version 3.2.0 (2015-04-16) -- \"Full of Ingredients\""
+R_VERSION_EXPECTED="R version 3.2.3 (2015-12-10) -- \"Wooden Christmas-Tree\""
 R_VERSION=$(R --version | egrep "R version")
 #printf "$R_VERSION" | xxd -
 
-if [[ ! "$R_VERSION" = "$R_VERSION_EXPECTED" ]] ; then
+if [[ ! "$R_VERSION" == "$R_VERSION_EXPECTED" ]] ; then
   echo "Expected '$R_VERSION_EXPECTED', but found '$R_VERSION'"
   exit 1
 fi
