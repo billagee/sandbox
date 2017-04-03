a0ef9171fde956cc1a1d3a9ada27d75670a82f70
Bill Agee <billagee@gmail.com>
2017-04-02 19:46:17 -0700

    Checking in IntelliJ project files

--- selenium/java/src/Selenium2Example.java
+++ selenium/java/src/Selenium2Example.java
@@ -1,53 +1,28 @@
-Create a new dir and create the pom.xml example from http://www.seleniumhq.org/docs/03_webdriver.jsp#java
-
-```
-<?xml version="1.0" encoding="UTF-8"?>
-<project xmlns="http://maven.apache.org/POM/4.0.0"
-                 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                 xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
-        <modelVersion>4.0.0</modelVersion>
-        <groupId>MySel20Proj</groupId>
-        <artifactId>MySel20Proj</artifactId>
-        <version>1.0</version>
-        <dependencies>
-            <dependency>
-                <groupId>org.seleniumhq.selenium</groupId>
-                <artifactId>selenium-server</artifactId>
-                <version>3.0.1</version>
-            </dependency>
-        </dependencies>
-</project>
-```
-
-Install maven and use it to install the Selenium bindings:
-
-```
-brew install maven
-
-mvn clean install
-```
-
-Import maven project in IntelliJ
-
-Create src/Selenium2Example.java and paste in example code from http://www.seleniumhq.org/docs/03_webdriver.jsp#introducing-the-selenium-webdriver-api-by-example
-
-
-```
-package org.openqa.selenium.example;
+//package org.openqa.selenium.example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Selenium2Example  {
-    public static void main(String[] args) {
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Selenium2Example {
+    public static void main(String[] args) throws MalformedURLException {
         // Create a new instance of the Firefox driver
-        // Notice that the remainder of the code relies on the interface, 
+        // Notice that the remainder of the code relies on the interface,
         // not the implementation.
-        WebDriver driver = new FirefoxDriver();
+        //WebDriver driver = new FirefoxDriver();
+        //WebDriver driver = new ChromeDriver();
+        DesiredCapabilities cap = DesiredCapabilities.chrome();
+        WebDriver driver = new RemoteWebDriver(
+                new URL("http://localhost:4444/wd/hub"), cap);
 
         // And now use this to visit Google
         driver.get("http://www.google.com");
@@ -65,7 +40,7 @@ public class Selenium2Example  {
 
         // Check the title of the page
         System.out.println("Page title is: " + driver.getTitle());
-        
+
         // Google's search is rendered dynamically with JavaScript.
         // Wait for the page to load, timeout after 10 seconds
         (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
@@ -76,11 +51,8 @@ public class Selenium2Example  {
 
         // Should see: "cheese! - Google Search"
         System.out.println("Page title is: " + driver.getTitle());
-        
+
         //Close the browser
         driver.quit();
     }
 }
-```
-
-Right-click Selenium2Example.java and click Run or Debug to create a run configuration.
