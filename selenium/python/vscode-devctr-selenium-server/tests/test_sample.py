521646e557c2e8d41abca88b2d001524faae4223
Bill Agee <billagee@gmail.com>
2020-10-19 14:40:41 -0700

    seleniumhq se server project

---
+++ selenium/python/vscode-devctr-selenium-server/tests/test_sample.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+@pytest.fixture(scope="function")
+def chrome():
+    driver = webdriver.Remote(
+        command_executor='http://selenium-hub:4444/wd/hub',
+        desired_capabilities=DesiredCapabilities.CHROME)
+    yield driver
+    driver.quit()
+
+class TestClass:
+    def test_one(self, chrome):
+        chrome.get("https://selenium.dev/")
+        assert "selenium" in chrome.current_url
